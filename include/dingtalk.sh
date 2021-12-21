5efd65b5b82db2eb3d9030eb9aa16583f23f8229
yangliuancn <yangliuan@foxmail.com>
2021-12-21 23:16:19 +0800

    fix

--- include/dingtalk.sh
+++ include/dingtalk.sh
@@ -2,6 +2,10 @@
 InstallDingtalk(){
     pushd ${start_dir}/src > /dev/null
     echo "Download dingtalk ..."
-    src_url="http://mirror.yangliuan.cn/dingtalk-2.1.13-latest-x86_64.AppImage"
+    src_url="http://mirror.yangliuan.cn/dingtalk-2.1.13-latest-x86_64.AppImage" && Download_src
+    sudo mkdir /opt/dingtalk
+    sudo mv -fv dingtalk-2.1.13-latest-x86_64.AppImage /opt/dingtalk/dingtalk.AppImage
+    sudo cp -rfv ${start_dir}/icon/dingtalk.png /opt/dingtalk
+    sudo cp -rfv ${start_dir}/desktop/dingtalk.desktop /usr/share/applications/
     popd > /dev/null
 }
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ sudo apt update && sudo apt upgrade
 start_dir=$(dirname "`readlink -f $0`")
 pushd ${start_dir} > /dev/null
 . ./version.txt
-. ./include/remove-liboffice.sh
+. ./include/remove_unwanted.sh
+. ./include/remove_liboffice.sh
 . ./include/chrome.sh
 . ./include/dingtalk.sh
 . ./include/wps.sh
