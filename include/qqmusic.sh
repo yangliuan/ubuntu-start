12f1e41d759482d940111d48dcd9e217e2b290bb
yangliuan <yangliuancn@foxmail.com>
2021-12-22 13:52:16 +0800

    add

--- include/qqmusic.sh
+++ include/qqmusic.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 InstallQQmusic(){
-    
+    pushd ${start_dir}/src > /dev/null
+    echo "Download qqmusic ..."
+    src_url="https://dldir1.qq.com/music/clntupate/linux/deb/qqmusic_1.1.1_amd64.deb" && Download_src
+    sudo apt dpkg -i qqmusic_1.1.1_amd64.deb
+    sudo apt install -f
+    sudo rm -rfv qqmusic_1.1.1_amd64.deb
+    popd > /dev/null
 }
