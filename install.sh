74f1c716703efa2713788c58c8d0618d82fff96d
yangliuan <yangliuancn@foxmail.com>
2021-12-22 13:58:17 +0800

    add

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ sudo apt update && sudo apt upgrade
 start_dir=$(dirname "`readlink -f $0`")
 pushd ${start_dir} > /dev/null
 . ./version.txt
+. ./download.sh
 . ./include/remove_unwanted.sh
 . ./include/remove_liboffice.sh
 . ./include/chrome.sh
 . ./include/dingtalk.sh
 . ./include/wps.sh
+. ./include/sougoupinyin.sh
+
+InstallSougoupinyin
