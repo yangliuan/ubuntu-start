831b58ffcefc5c6da6945855b3bc852a316c7b0e
yangliuan <yangliuancn@foxmail.com>
2021-12-24 14:58:53 +0800

    modify

--- include/sunlogin.sh
+++ include/sunlogin.sh
@@ -3,8 +3,8 @@ InstallSunlogin(){
     pushd ${start_dir}/src > /dev/null
     echo "Download sunlogin ..."
     src_url="https://down.oray.com/sunlogin/linux/sunloginclient-11.0.0.36662-amd64.deb" && Download_src
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo dpkg -i sunloginclient-11.0.0.36662-amd64.deb
     sudo apt install -f
-    sudo rm -rfv google-chrome-stable_current_amd64.deb
+    sudo rm -rfv sunloginclient-11.0.0.36662-amd64.deb
     popd > /dev/null
 }
