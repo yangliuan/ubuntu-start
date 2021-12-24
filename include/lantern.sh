d4f46cafb380ecd8ca63750c4f403c77ee333f55
yangliuan <yangliuancn@foxmail.com>
2021-12-24 18:56:16 +0800

    add

--- include/lantern.sh
+++ include/lantern.sh
@@ -2,7 +2,7 @@
 InstallLantern(){
     pushd ${start_dir}/src > /dev/null
     echo "Download lantern ..."
-    src_url="https://github.com/getlantern/lantern-binaries/raw/master/lantern-installer-64-bit.deb" && Download_src
+    src_url="http://mirror.yangliuan.cn/lantern-installer-64-bit.deb" && Download_src
     sudo dpkg -i lantern-installer-64-bit.deb
     sudo apt install -f
     sudo rm -rfv lantern-installer-64-bit.deb
