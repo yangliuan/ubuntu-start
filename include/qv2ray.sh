4f6356aa44802529db519e06195cdb79b07b22d1
yangliuancn <yangliuan@foxmail.com>
2021-12-25 12:32:58 +0800

    modify

--- include/qv2ray.sh
+++ include/qv2ray.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 InstallQv2ray(){
-    echo 'q2ray'
+    pushd ${start_dir}/src > /dev/null
+    echo "Download qv2ray ..."
+    src_url="http://mirror.yangliuan.cn/qv2ray.zip" && Download_src
+    unzip qv2ray.zip
+
+    if [ ! -e "~/.config/qv2ray/" ]; then
+      mkdir ~/.config/qv2ray/
+    fi
+
+    sudo mv -fv qv2ray/ /opt/qv2ray
+    sudo mv -fv qv2ray/vcore ~/.config/qv2ray/
+    sudo cp -rfv ${start_dir}/desktop/qv2ray.desktop /usr/share/applications/
+    sudo rm -rf qv2ray.zip
+    popd > /dev/null
 }
--- include/v2rayL.sh
+++ include/v2rayL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 InstallV2rayL(){
     pushd ${start_dir}/src > /dev/null
-    bash <(curl -s -L http://dl.thinker.ink/install.sh)
+    
     popd > /dev/null
 }
