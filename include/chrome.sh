4d9fde8f88f6d40733868492364e2d72007576be
yangliuancn <yangliuan@foxmail.com>
2021-12-26 23:01:36 +0800

    add

--- include/chrome.sh
+++ include/chrome.sh
@@ -2,7 +2,7 @@
 InstallChrome(){
     pushd ${start_dir}/src > /dev/null
     echo "Download chrome ..."
-    src_url="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" && Download_src
+    src_url="http://mirror.yangliuan.cn/google-chrome-stable_current_amd64.deb" && Download_src
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     sudo apt-get install -f
     sudo rm -rfv google-chrome-stable_current_amd64.deb
--- include/flameshot.sh
+++ include/flameshot.sh
@@ -2,7 +2,7 @@
 InstallFlameshot(){
     pushd ${start_dir}/src > /dev/null
     echo "Download flameshot ..."
-    src_url="https://github.com/flameshot-org/flameshot/releases/download/v0.10.2/flameshot-0.10.2-1.debian-10.amd64.deb" && Download_src
+    src_url="http://mirror.yangliuan.cn/flameshot-0.10.2-1.debian-10.amd64.deb" && Download_src
     sudo dpkg -i flameshot-0.10.2-1.debian-10.amd64.deb
     sudo apt-get install -f
     sudo rm -rfv flameshot-0.10.2-1.debian-10.amd64.deb
