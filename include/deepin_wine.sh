8d2375c778bb84e65100373b31093dc527410d13
yangliuancn <yangliuancn@foxmail.com>
2021-12-27 13:46:40 +0800

    modify

--- include/deepin_wine.sh
+++ include/deepin_wine.sh
@@ -3,4 +3,7 @@ InstallDeepinWine(){
     wget -O- https://deepin-wine.i-m.dev/setup.sh | sh
     sudo apt-get install com.qq.weixin.deepin
     sudo apt-get install -f
+    #解决无法发送图片问题
+    sudo apt-get install libjpeg62:i386
+    sudo apt-get install libjpeg62
 }
