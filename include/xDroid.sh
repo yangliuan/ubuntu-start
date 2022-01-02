ae5b4bfc92cbbd3a1530877c25f12feb517f22e8
yangliuancn <yangliuan@foxmail.com>
2022-01-02 19:18:16 +0800

    modify

--- include/xDroid.sh
+++ include/xDroid.sh
@@ -7,8 +7,8 @@ InstallxDroid(){
     echo "Download xDroid ..."
     src_url="https://d6.injdk.cn/xdroid/xDroidInstall-x86_64-v${xDroid_ver}.tar.gz" && Download_src
     tar -zxvf xDroidInstall-x86_64-v${xDroid_ver}.tar.gz
-    ./xDroidInstall-x86_64-v${xDroid_ver}.run
-    rm -rf xDroidInstall-x86_64-v${xDroid_ver}.tar.gz xDroidInstall-x86_64-v${xDroid_ver}.run
+    sudo -u ${run_user} ./xDroidInstall-x86_64-v${xDroid_ver}.run
+    sudo rm -rf xDroidInstall-x86_64-v${xDroid_ver}.tar.gz xDroidInstall-x86_64-v${xDroid_ver}.run
 
     popd > /dev/null
 }
