c5c0547466129c2265ec46eb6a7dcccb779375d4
yangliuan <yangliuancn@foxmail.com>
2022-01-30 16:12:25 +0800

    modify

--- include/remove_deepinwine.sh
+++ include/remove_deepinwine.sh
@@ -1,4 +1,5 @@
+sudo aptpurge-get --autoremove com.qq.weixin.deepin
+sudo aptpurge-get --autoremove com.qq.weixin.work.deepin
 sudo rm -rf ~/Documents/WeChat\ Files ~/Documents/Tencent\ Files
 sudo rm /etc/apt/preferences.d/deepin-wine.i-m.dev.pref /etc/apt/sources.list.d/deepin-wine.i-m.dev.list /etc/profile.d/deepin-wine.i-m.dev.sh
-sudo apt-get purge --autoremove com.qq.weixin.deepin
 sudo apt-get update
