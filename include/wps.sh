d15384f6f267f8940b3ccdde0251cc879dcc5ce3
yangliuancn <yangliuan@foxmail.com>
2022-02-06 14:40:33 +0800

    modify version

--- include/wps.sh
+++ include/wps.sh
@@ -2,9 +2,9 @@
 InstallWps(){
     pushd ${start_dir}/src > /dev/null
     echo "Download wps ..."
-    src_url="https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_amd64.deb" && Download_src
-    sudo dpkg -i wps-office_11.1.0.10702_amd64.deb
+    src_url="https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/${wps_sub_ver}/wps-office_${wps_ver}_amd64.deb" && Download_src
+    sudo dpkg -i wps-office_${wps_ver}_amd64.deb
     sudo apt-get install -f
-    sudo rm -rfv wps-office_11.1.0.10702_amd64.deb
+    sudo rm -rfv wps-office_${wps_ver}_amd64.deb
     popd > /dev/null
 }
