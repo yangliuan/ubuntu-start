fb6249e01e8701b1a3c1ff2caf170444555a4b2a
yangliuancn <yangliuan@foxmail.com>
2022-06-29 16:55:38 +0800

    add

--- include/wps.sh
+++ include/wps.sh
@@ -3,7 +3,8 @@ Install_Wps() {
     pushd ${start_dir}/src > /dev/null
 
     echo "Download wps ..."
-    src_url="https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/${wps_sub_ver}/wps-office_${wps_ver}_amd64.deb" && Download_src
+    src="https://wps-linux-personal.wpscdn.cn/wps/download/ep/Linux2019/${wps_sub_ver}/wps-office_${wps_ver}_amd64.deb" && 
+    #src_url="https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/${wps_sub_ver}/wps-office_${wps_ver}_amd64.deb" && Download_src
     dpkg -i wps-office_${wps_ver}_amd64.deb
     apt-get install -f
     rm -rfv wps-office_${wps_ver}_amd64.deb
