8f8a0f781cc838665d44bf93fd9005bd1d891d79
yangliuancn <yangliuan@foxmail.com>
2022-08-12 22:04:51 +0800

    md

--- include/theme_tools.sh
+++ include/theme_tools.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 Install_ThemeTools() {
-    apt-get install gnome-tweak-tool
+    if [ "${Ubuntu_ver}" == "22" ]; then
+        apt-get install gnome-tweak
+    else
+        apt-get install gnome-tweak-tool
+    fi
+    
     apt-get install chrome-gnome-shell
     apt-get install gnome-shell-extensions
 }
 
 Uninstall_ThemeTools() {
-    apt-get autoremove gnome-tweak-tool
+    if [ "${Ubuntu_ver}" == "22" ]; then
+        apt-get autoremove gnome-tweak
+    else
+        apt-get autoremove gnome-tweak-tool
+    fi
+    
     apt-get autoremove chrome-gnome-shell
     apt-get autoremove gnome-shell-extensions
 }
