27a022e5b87ff61eb670a8d420c649be4e70e8b0
yangliuancn <yangliuan@foxmail.com>
2022-08-13 20:14:01 +0800

    md

--- include/appimage_suport.sh
+++ include/appimage_suport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Install_AppImageSuport() {
-    apt install fuse libfuse2
+    apt install fuse libfuse2 2> /dev/null
     modprobe fuse
     groupadd fuse
     usermod -a -G fuse ${run_user}
