cf3807fcae118d72855230a9441563da9a00d9c7
yangliuancn <yangliuan@foxmail.com>
2022-08-13 19:41:09 +0800

    md

--- include/indicator_sysmonitor.sh
+++ include/indicator_sysmonitor.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 Install_IndicatorSysmonitor() {
     apt install python3-psutil gir1.2-appindicator3-0.1
-    git clone https://github.com/fossfreedom/indicator-sysmonitor.git
+    pushd ${start_dir}/src > /dev/null
+    src_url="http://mirror.yangliuan.cn/indicator-sysmonitor.tar.xz" && Download_src
+    tar xJf indicator-sysmonitor.tar.xz
     pushd indicator-sysmonitor > /dev/null
     make install
     popd > /dev/null
     rm -rf indicator-sysmonitor
+    popd > /dev/null
     nohup indicator-sysmonitor &
 }
 
