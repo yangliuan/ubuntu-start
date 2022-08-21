81e93ce37469b3f6807418a50a59947117705b14
yangliuancn <yangliuan@foxmail.com>
2022-08-21 12:38:46 +0800

    modify xDroid

--- include/xDroid.sh
+++ include/xDroid.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 Install_xDroid() {
     #安卓模拟器
-    #DOC:https://www.linzhuotech.com/Public/Home/img/gitbook/user_manual_nv/_book/xDroid%E6%89%8B%E5%86%8C/2-2.html
+    #DOC:https://www.linzhuotech.com/Product/download
     pushd ${start_dir}/src > /dev/null
     
     echo "Download xDroid ..."
-    src_url="https://d6.injdk.cn/xdroid/xDroidInstall-x86_64-v${xDroid_ver}.zip" && Download_src
-    unzip xDroidInstall-x86_64-v${xDroid_ver}.zip
+    src_url="https://d6.injdk.cn/xdroid/xDroidInstall-x86_64-v${xDroid_ver}.run.zip" && Download_src
+    unzip xDroidInstall-x86_64-v${xDroid_ver}.run.zip
     sudo -u ${run_user} ./xDroidInstall-x86_64-v${xDroid_ver}.run
     rm -rf xDroidInstall-x86_64-v${xDroid_ver}.zip xDroidInstall-x86_64-v${xDroid_ver}.run
 
