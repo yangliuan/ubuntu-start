2067f0f087ca6a2a4434ac95ff344ad4c8f43963
yangliuan <yangliuancn@foxmail.com>
2022-09-07 13:18:45 +0800

    md

--- include/remove_liboffice.sh
+++ include/remove_liboffice.sh
@@ -7,4 +7,6 @@ Remove_Liboffice(){
     apt-get remove libreoffice*
     apt-get purge libreoffice*
     apt-get autoremove
+    #防止gnome依赖出现问题导致设置中心无法使用
+    apt-get install gnome-control-center --reinstall
 }
