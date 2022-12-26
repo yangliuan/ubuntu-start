7f5079c2048549ab6cff263f5d5b1fed2ae3bed0
yangliuan <yangliuancn@foxmail.com>
2022-12-26 17:50:20 +0800

    md

--- include/xDroid.sh
+++ include/xDroid.sh
@@ -5,10 +5,10 @@ Install_xDroid() {
     pushd ${start_dir}/src > /dev/null
     
     echo "Download xDroid ..."
-    src_url="https://d6.injdk.cn/xdroid/xDroidInstall-x86_64-v${xDroid_ver}.run.zip" && Download_src
-    unzip xDroidInstall-x86_64-v${xDroid_ver}.run.zip
+    src_url="https://d6.injdk.cn/xdroid/xDroidInstall-x86_64-v${xDroid_ver}.tar.gz" && Download_src
+    tar -zxvf xDroidInstall-x86_64-v${xDroid_ver}.run.tar.gz
     sudo -u ${run_user} ./xDroidInstall-x86_64-v${xDroid_ver}.run
-    rm -rf xDroidInstall-x86_64-v${xDroid_ver}.zip xDroidInstall-x86_64-v${xDroid_ver}.run
+    rm -rf xDroidInstall-x86_64-v${xDroid_ver}.run.tar.gz xDroidInstall-x86_64-v${xDroid_ver}.run
 
     popd > /dev/null
 }
