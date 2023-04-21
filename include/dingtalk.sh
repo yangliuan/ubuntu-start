6b4c26dfbebeb7df8b64f553f0edde55f53dbde3
yangliuan <yangliuancn@foxmail.com>
2023-04-21 17:13:56 +0800

    md

--- include/dingtalk.sh
+++ include/dingtalk.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+#https://page.dingtalk.com/wow/z/dingtalk/simple/ddhomedownlaod#/
 Install_Dingtalk() {
-    #官方版 https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_1.3.0.20214_amd64.deb
     pushd ${start_dir}/src > /dev/null
-
     echo "Download dingtalk ..."
     src_url="https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_${dingtalk_ver}_amd64.deb" && Download_src
     dpkg -i com.alibabainc.dingtalk_${dingtalk_ver}_amd64.deb
     apt-get install -f
-    rm -rfv com.alibabainc.dingtalk_${dingtalk_ver}_amd64.deb
-
+    #rm -rfv com.alibabainc.dingtalk_${dingtalk_ver}_amd64.deb
     popd > /dev/null
 }
 
