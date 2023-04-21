b92419cc7127640bd6de9f1aeec1667bc2f8d269
yangliuan <yangliuancn@foxmail.com>
2023-04-21 17:51:29 +0800

    md

--- include/sunlogin.sh
+++ include/sunlogin.sh
@@ -3,7 +3,8 @@
 Install_Sunlogin() {
     pushd ${start_dir}/src > /dev/null
     echo "Download sunlogin ..."
-    src_url="https://down.oray.com/sunlogin/linux/SunloginClient_${sunlogin_ver}-amd64.deb" && Download_src
+    
+    src_url="https://down.oray.com/sunlogin/linux/SunloginClient_${sunlogin_ver}_amd64.deb" && Download_src
     dpkg -i SunloginClient_${sunlogin_ver}-amd64.deb
     apt-get install -f
     rm -rfv SunloginClient_${sunlogin_ver}-amd64.deb
